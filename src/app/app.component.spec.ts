c03779c1f9da2720053a449a16f78412dbb65403
AgnieszkaKurek <agniechakurek@gmail.com>
2019-08-13 10:43:37 +0200

    Revert "installing missing modules"
    This reverts commit cab7632d2e2a78c6bc91dc1e1467fe83c15e71fd.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TiCTacToeGameAngular'`, () => {
+  it(`should have as title 'TicTacToeGameAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TiCTacToeGameAngular');
+    expect(app.title).toEqual('TicTacToeGameAngular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TiCTacToeGameAngular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TicTacToeGameAngular!');
   });
 });
