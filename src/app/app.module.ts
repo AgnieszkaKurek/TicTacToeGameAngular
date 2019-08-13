dd039bce281879ca0add68c41cd5c46c47438b69
AgnieszkaKurek <agniechakurek@gmail.com>
2019-08-13 10:43:13 +0200

    Revert "add component board"
    This reverts commit a254ee85c599f749a5245ce567f06ead8d47479b.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
