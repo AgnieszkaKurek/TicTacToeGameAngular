50e1a6855363b88a90add86b909f2553fb5225e8
AgnieszkaKurek <agniechakurek@gmail.com>
2019-08-23 06:20:45 +0200

    corrected incorrect file path

--- src/app/tic-tac-toe-game-score.service.ts
+++ src/app/tic-tac-toe-game-score.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { TicTacToeGameStatus } from './tic-tac-toe-game-status.enum.js';
+import { TicTacToeGameStatus } from './moduls/tic-tac-toe-game-status.enum.js';
 
 @Injectable({
   providedIn: 'root'
