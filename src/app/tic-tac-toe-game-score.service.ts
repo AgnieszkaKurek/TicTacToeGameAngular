3f05d63f5fefc5d65c5384aff95ee9b2d5e32e89
AgnieszkaKurek <agniechakurek@gmail.com>
2019-08-26 09:37:57 +0200

    removal of constructor

--- src/app/tic-tac-toe-game-score.service.ts
+++ src/app/tic-tac-toe-game-score.service.ts
@@ -6,13 +6,9 @@ import { TicTacToeGameStatus } from './models/tic-tac-toe-game-status.enum';
 })
 export class TicTacToeGameScoreService {
 
-  public constructor(
-    public scorePlayerX: number = 0,
-    public scorePlayerO: number = 0,
-    public numberOfDraws: number = 0,
-  ) {
-
-  }
+  public scorePlayerX = 0;
+  public scorePlayerO = 0;
+  public numberOfDraws = 0;
 
   public update(status: TicTacToeGameStatus): void {
     if (status === TicTacToeGameStatus.X_WINS) {
