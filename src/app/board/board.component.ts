e56d3873b5e25277c5d246b1bca8631347295502
AgnieszkaKurek <agniechakurek@gmail.com>
2019-09-23 14:12:05 +0200

    created game bord

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TicTacToeGameService } from '../tic-tac-toe-game.service';
 
 @Component({
   selector: 'ttt-board',
-  templateUrl: './board.component.html'
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+  constructor(public game: TicTacToeGameService) { }
 }
--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ttt-box',
-  templateUrl: './box.component.html'
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.scss']
 })
 export class BoxComponent {
 
