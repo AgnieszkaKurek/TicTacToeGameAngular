d0514ce94c71c9525366fce6b1ae52d41725029c
AgnieszkaKurek <agniechakurek@gmail.com>
2019-10-09 16:37:50 +0200

    handle click

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,16 +1,19 @@
+import { TicTacToeGameScoreService } from './../tic-tac-toe-game-score.service';
 import { TicTacToeGamePlayer } from './../models/tic-tac-toe-game-player.enum';
 import { TicTacToeGameService } from './../tic-tac-toe-game.service';
-import { Component, Input, HostListener } from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
+import { TicTacToeGameStatus } from '../models/tic-tac-toe-game-status.enum';
 
 @Component({
   selector: 'ttt-box',
   templateUrl: './box.component.html',
   styleUrls: ['./box.component.scss']
 })
-export class BoxComponent {
+export class BoxComponent implements OnInit {
 
   public constructor(
-    public game: TicTacToeGameService,
+    private game: TicTacToeGameService,
+    private score: TicTacToeGameScoreService,
   ) {
   }
 
@@ -21,10 +24,22 @@ export class BoxComponent {
 
   public playerO: TicTacToeGamePlayer = TicTacToeGamePlayer.O;
 
-  public status = this.game.board[this.position];
+  public status: TicTacToeGamePlayer;
 
-  @HostListener('click')
-  public onClick() {
+   @HostListener('click')
+  public handleGameMove(): void {
+    if (this.game.status() === TicTacToeGameStatus.UNFINISHED) {
+      this.game.move(this.position);
+      this.score.update(this.game.status());
+      this.setStatus();
+    }
+  }
 
+  public ngOnInit(): void {
+    this.setStatus();
+  }
+
+  private setStatus(): void {
+    this.status = this.game.board[this.position];
   }
 }
