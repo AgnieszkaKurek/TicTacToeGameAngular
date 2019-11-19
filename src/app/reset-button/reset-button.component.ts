bf4c518c7da30bdd8656f750afe5de966018ec16
AgnieszkaKurek <agniechakurek@gmail.com>
2019-11-19 14:47:30 +0100

    add  start new  game  button

--- src/app/reset-button/reset-button.component.ts
+++ src/app/reset-button/reset-button.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { TicTacToeGameStatus } from './../models/tic-tac-toe-game-status.enum';
+import { TicTacToeGameService } from './../tic-tac-toe-game.service';
 
 @Component({
   selector: 'ttt-reset-button',
-  templateUrl: './reset-button.component.html'
+  templateUrl: './reset-button.component.html',
+  styleUrls: ['./reset-button.component.scss']
 })
 export class ResetButtonComponent {
+  public status = {
+    x: TicTacToeGameStatus.X_WINS,
+    o: TicTacToeGameStatus.O_WINS,
+    draw: TicTacToeGameStatus.DRAW,
+  };
+  public constructor(
+    public game: TicTacToeGameService,
+  ) {
+  }
 }
