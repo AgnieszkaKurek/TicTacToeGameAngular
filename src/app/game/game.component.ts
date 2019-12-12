025cbcbfa3f2348d9ae6b5fcddb79a87a501d1d3
AgnieszkaKurek <agniechakurek@gmail.com>
2019-12-12 18:12:02 +0100

    add responsive design for game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ttt-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.behavior.scss',
+    './game.component.rvd.scss'
+  ]
 })
 export class GameComponent {
 }
